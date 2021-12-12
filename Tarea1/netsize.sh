d3e2d41f5d57de3a23947f9eb9c145e4ed04688a
NayeliMG <94936301+NayeliMG@users.noreply.github.com>
2021-12-11 21:08:56 -0500

    Update netsize.sh
    Se colocó el código de "head" para contar el número de columnas

--- Tarea1/netsize.sh
+++ Tarea1/netsize.sh
@@ -1,4 +1,6 @@
 # Ejercicio 1.10.3 parte 1
   # Escriba un scrip que tome uno de estos archivos y determine el número de filas (polinizadroes) y columnas (plantas).
 # Comandos
-  
+  - Para contar el número de columnas usamos el comando "head" de la siguiente manera:
+    head -n 1 ../Saavedra2013/n5.txt | wc -l 
+    
