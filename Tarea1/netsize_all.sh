365fb7cd564918c8dcf763b1248ee586c5f8de42
NayeliMG <94936301+NayeliMG@users.noreply.github.com>
2021-12-11 22:24:07 -0500

    Update netsize_all.sh
    Se colocó el código para "grep"

--- Tarea1/netsize_all.sh
+++ Tarea1/netsize_all.sh
@@ -2,4 +2,6 @@
   - Escribir un scrip que imprima el número de filas y columnas de cada red:
   - Hacemos uso del comado "for" de la siguiente manera:
     for file in $(ls *.txt); do head -n 1 $file; done
+  - hacemos uso del comado "grep"
+    for file in $(ls *.txt); head -n 1 $file | grep -o " " | wc -l; done
    
