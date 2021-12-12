58307eccc7612d76d5e2f7a02ddb429edba61d07
NayeliMG <94936301+NayeliMG@users.noreply.github.com>
2021-12-11 22:18:49 -0500

    Update netsize_all.sh
    Se añadió el código para "for"

--- Tarea1/netsize_all.sh
+++ Tarea1/netsize_all.sh
@@ -1,3 +1,5 @@
 # Ejercicio 1.10.3 parte 2
   - Escribir un scrip que imprima el número de filas y columnas de cada red:
-  
+  - Hacemos uso del comado "for" de la siguiente manera:
+    for file in $(ls *.txt); do head -n 1 $file; done
+   
